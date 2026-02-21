5c3886143223dcd5db18dc9e9cb2797440351af0
Edigi Akshay Goud <edigiakshaygoud@gmail.com>
2026-02-21 19:03:56 +0530

    Time: 5 ms (12.54%) | Memory: 9.6 MB (85.98%) - LeetSync

--- 4186-count-residue-prefixes/count-residue-prefixes.cpp
+++ 4186-count-residue-prefixes/count-residue-prefixes.cpp
@@ -1,29 +1,49 @@
 class Solution {
 public:
-    int duplicates(string str){
-        set<char>st;
-        for(int i=0;i<str.length();i++){
-            char ch = str[i];
-            st.insert(ch);
-        }
-        return st.size();
-    }
     int residuePrefixes(string s) {
-        int k = s.length();
-        if(k==1){
-            return 1;
+    vector<int>freq(26,0);
+    int count = 0;
+    int uniquecount = 0;
+    for(int i=0;i<s.length();i++){
+        int idx = s[i]-'a';
+        if(freq[idx] == 0){
+            uniquecount++;
         }
-        vector<string>prefix(k);
-        prefix[0] = s[0];
-        int count = 1;
-        for(int i=1;i<k;i++){
-            prefix[i] = prefix[i-1]+s[i];
-            int n = prefix[i].length();
-            int m = duplicates(prefix[i]);
-            if(m == n%3){
-                count++;
-            }
+        freq[idx]++;
+        int prefixlen = i+1;
+        if(uniquecount == (prefixlen%3)){
+            count++;
         }
+    }
         return count;
     }
 };
+// class Solution {
+// public:
+//     int duplicates(string str){
+//         set<char>st;
+//         for(int i=0;i<str.length();i++){
+//             char ch = str[i];
+//             st.insert(ch);
+//         }
+//         return st.size();
+//     }
+//     int residuePrefixes(string s) {
+//         int k = s.length();
+//         if(k==1){
+//             return 1;
+//         }
+//         vector<string>prefix(k);
+//         prefix[0] = s[0];
+//         int count = 1;
+//         for(int i=1;i<k;i++){
+//             prefix[i] = prefix[i-1]+s[i];
+//             int n = prefix[i].length();
+//             int m = duplicates(prefix[i]);
+//             if(m == n%3){
+//                 count++;
+//             }
+//         }
+//         return count;
+//     }
+// };
