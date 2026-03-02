065260f1ab22528a091f823a263da00f3f780ede
Edigi Akshay Goud <edigiakshaygoud@gmail.com>
2026-03-02 17:50:19 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (70.00%) - LeetSync

--- 4229-trim-trailing-vowels/trim-trailing-vowels.cpp
+++ 4229-trim-trailing-vowels/trim-trailing-vowels.cpp
@@ -2,27 +2,15 @@ class Solution {
 public:
     string trimTrailingVowels(string s) {
         int n = s.length()-1;
-        string s1 = "";
         while(n>=0){
-            char c = s.back();
+            char c = s[n];
             if(c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u'){
-                s = s.substr(0,n);
                 n--;
             }
             else{
-                return s;
+                return s.substr(0,n+1);
             }   
         }
-        // for(int i=n-1;i>=0;i--){
-        //     char c= s[i];
-        //     if(c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u'){
-        //         continue;
-        //     }
-        //     else{
-        //         s1 = c + s1;
-        //     }
-        // }
-        //return s1;
-        return s;
+        return s.substr(0,n+1);
     }
 };
