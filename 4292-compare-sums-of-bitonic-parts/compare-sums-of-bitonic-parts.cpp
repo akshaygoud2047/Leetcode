bfdaa4bea6c9d5b0982766d3c1344b69836eea4b
Edigi Akshay Goud <195651403+akshaygoud2047@users.noreply.github.com>
2026-04-29 19:13:40 +0530

    Time: 0 ms (100.00%) | Memory: 125.6 MB (64.69%) - LeetSync

--- 4292-compare-sums-of-bitonic-parts/compare-sums-of-bitonic-parts.cpp
+++ 4292-compare-sums-of-bitonic-parts/compare-sums-of-bitonic-parts.cpp
@@ -1,38 +1,14 @@
 class Solution {
 public:
     int compareBitonicSums(vector<int>& nums) {
-        long int sum1 = 0;
-        long int sum2 = 0;
-        int n = nums.size();
+        long long a=nums[0],b=nums.back(),n=nums.size();
 
-        int peak = -1;
-
-        // Find peak index (where increasing stops)
-        for (int i = 0; i < n - 1; i++) {
-            if (nums[i+1] < nums[i]) {
-                peak = i;
-                break;
-            }
+        for(int i=1;i<n;i++){
+            if(nums[i-1] < nums[i]) a+=nums[i];
+            else if(nums[i-1] > nums[i]) b+=nums[i-1];
         }
-
-        // If no break happened → peak is last index
-        if (peak == -1) {
-            peak = n - 1;
-        }
-
-        // Sum from start to peak
-        for (int i = 0; i <= peak; i++) {
-            sum1 += nums[i];
-        }
-
-        // Sum from peak to end
-        for (int i = peak; i < n; i++) {
-            sum2 += nums[i];
-        }
-
-        // Compare
-        if (sum1 == sum2) return -1;
-        else if (sum1 < sum2) return 1;
-        return 0;
+        if(a > b) return 0;
+        else if(a < b) return 1;
+        else return -1;
     }
 };
