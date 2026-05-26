b1ed3c9414267c4a96903644aaba83bddcde264e
Edigi Akshay Goud <195651403+akshaygoud2047@users.noreply.github.com>
2026-05-26 22:22:41 +0530

    Time: 3 ms (29.00%) | Memory: 9.2 MB (47.56%) - LeetSync

--- 3408-count-the-number-of-special-characters-i/count-the-number-of-special-characters-i.cpp
+++ 3408-count-the-number-of-special-characters-i/count-the-number-of-special-characters-i.cpp
@@ -1,11 +1,6 @@
 class Solution {
 public:
     int numberOfSpecialChars(string word) {
-        map<char,int>hash;
-        int n = word.length();
-        for(int i=0;i<n;i++){
-            hash[word[i]]++;
-        }
         int count = 0;
         set<char>wrd(word.begin(),word.end());
         for(char ch : wrd){
