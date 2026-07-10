db5bce54f91114c6f30bcea4a576b96ed5d8ea77
Edigi Akshay Goud <195651403+akshaygoud2047@users.noreply.github.com>
2026-07-10 19:23:24 +0530

    Time: 12 ms (96.06%) | Memory: 60.3 MB (92.74%) - LeetSync

---
+++ 630-course-schedule-iii/course-schedule-iii.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int scheduleCourse(vector<vector<int>>& courses) {
+
+        sort(courses.begin(), courses.end(),
+             [](vector<int>& a, vector<int>& b) {
+                 return a[1] < b[1];
+             });
+
+        priority_queue<int> pq;   // Max heap of durations
+        int time = 0;
+
+        for (auto &course : courses) {
+
+            int duration = course[0];
+            int lastDay = course[1];
+
+            time += duration;
+            pq.push(duration);
+
+            if (time > lastDay) {
+                time -= pq.top();
+                pq.pop();
+            }
+        }
+
+        return pq.size();
+    }
+};
