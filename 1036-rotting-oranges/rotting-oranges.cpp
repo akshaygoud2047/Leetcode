a0b2efe7568adfc25ea21b35845a3b8f22f8d764
Edigi Akshay Goud <195651403+akshaygoud2047@users.noreply.github.com>
2026-07-21 19:27:43 +0530

    Time: 2 ms (31.38%) | Memory: 16.9 MB (46.41%) - LeetSync

---
+++ 1036-rotting-oranges/rotting-oranges.cpp
@@ -0,0 +1,68 @@
+class Solution {
+public:
+    typedef pair<int, int> P;
+
+    vector<vector<int>> directions = {
+        {-1, 0},
+        {1, 0},
+        {0, -1},
+        {0, 1}
+    };
+
+    int orangesRotting(vector<vector<int>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size();
+
+        queue<P> que;
+        int freshCount = 0;
+
+        // Count fresh oranges and store rotten oranges
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == 2) {
+                    que.push({i, j});
+                } 
+                else if (grid[i][j] == 1) {
+                    freshCount++;
+                }
+            }
+        }
+
+        // No fresh oranges
+        if (freshCount == 0) {
+            return 0;
+        }
+
+        int minutes = 0;
+
+        while (!que.empty()) {
+            int size = que.size();
+
+            while (size--) {
+                P curr = que.front();
+                que.pop();
+
+                int i = curr.first;
+                int j = curr.second;
+
+                for (auto &dir : directions) {
+                    int new_i = i + dir[0];
+                    int new_j = j + dir[1];
+
+                    if (new_i >= 0 && new_i < m &&
+                        new_j >= 0 && new_j < n &&
+                        grid[new_i][new_j] == 1) {
+
+                        grid[new_i][new_j] = 2;
+                        freshCount--;
+                        que.push({new_i, new_j});
+                    }
+                }
+            }
+
+            minutes++;
+        }
+
+        return (freshCount == 0) ? (minutes - 1) : -1;
+    }
+};
