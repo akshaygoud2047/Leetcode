e641a5571ad97021f8f9f73beacc1f80aa2cf070
Edigi Akshay Goud <195651403+akshaygoud2047@users.noreply.github.com>
2026-08-16 11:27:11 +0530

    Time: 4 ms (5.85%) | Memory: 55.3 MB (81.43%) - LeetSync

---
+++ 4378-nearest-available-drone/nearest-available-drone.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int nearestDrone(vector<vector<int>>& drones, vector<int>& target) {
+        int minDist = INT_MAX;
+        int ans = -1;
+
+        for (int i = 0; i < drones.size(); i++) {
+            int x = drones[i][0];
+            int y = drones[i][1];
+            int range = drones[i][2];
+
+            int dist = abs(x - target[0]) + abs(y - target[1]);
+
+            if (dist <= range && dist < minDist) {
+                minDist = dist;
+                ans = i;
+            }
+        }
+
+        return ans;
+    }
+};
