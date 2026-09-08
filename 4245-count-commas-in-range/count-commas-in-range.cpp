d129033f94243c89e5b0769b97ecbac2976e85e4
Edigi Akshay Goud <195651403+akshaygoud2047@users.noreply.github.com>
2026-09-08 17:31:53 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (51.38%) - LeetSync

--- 4245-count-commas-in-range/count-commas-in-range.cpp
+++ 4245-count-commas-in-range/count-commas-in-range.cpp
@@ -4,9 +4,7 @@ public:
         if(n<1000){
             return 0;
         }
-        if(n>1000 && n<9999){
-            return n-999;
-        }
+       
         else{
             return n-999;
         }
