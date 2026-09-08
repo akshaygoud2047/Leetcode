77c279f7486ba6242ded04a8b2f00f837d99a95a
Edigi Akshay Goud <195651403+akshaygoud2047@users.noreply.github.com>
2026-09-08 17:31:41 +0530

    Time: 1 ms (16.95%) | Memory: 8.6 MB (15.51%) - LeetSync

--- 4245-count-commas-in-range/count-commas-in-range.cpp
+++ 4245-count-commas-in-range/count-commas-in-range.cpp
@@ -1,7 +1,14 @@
 class Solution {
 public:
     int countCommas(int n) {
-        if(n<1000) return 0;
-        else return (n - 1000)+1;
+        if(n<1000){
+            return 0;
+        }
+        if(n>1000 && n<9999){
+            return n-999;
+        }
+        else{
+            return n-999;
+        }
     }
 };
